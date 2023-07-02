a17c4504c786e53334bb04ad7f3ed4e46ce358c2
Jorge Duarte <sadujorge@gmail.com>
2023-07-02 14:16:41 -0700

    Completed Category add route.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -17,13 +17,27 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
+router.get('/:id', async (req, res) => {
   // be sure to include its associated Products
+  try {
+    // find one category by its `id` value
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
