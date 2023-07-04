544cccf5d75479eef1047c655baeea11c440567b
Jorge Duarte <sadujorge@gmail.com>
2023-07-03 19:35:07 -0700

    Minor changes to product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -44,7 +44,7 @@ router.post('/', (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds && req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -111,8 +111,7 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
-    const productData = await Product.update(
-      { active: false },
+    const productData = await Product.destroy(
       { where: { id: req.params.id } }
     );
     if (productData[0] === 0) {
