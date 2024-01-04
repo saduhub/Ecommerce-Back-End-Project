c8dbccd0ea4dbd1937c003816ecab42d5381a663
Jorge Duarte <sadujorge@gmail.com>
2024-01-03 22:12:51 -0800

    Added get products event listener

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,24 @@
 "use strict";
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
+const productsLink = document.getElementById('productsLink');
+// Check if the element is indeed an HTMLAnchorElement (anchor tag)
+if (productsLink instanceof HTMLAnchorElement) {
+    productsLink.addEventListener('click', function (event) {
+        event.preventDefault();
+        fetch('/api/products')
+            .then((response) => {
+            if (!response.ok) {
+                throw new Error('Server Error');
+            }
+            return response.json();
+        })
+            .then((data) => {
+            console.log(data);
+        })
+            .catch((error) => {
+            console.error('Error:', error.message);
+        });
+    });
+}
+else {
+    console.error('Link (front-end) is not working!');
+}
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,24 @@
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
+const productsLink = document.getElementById('productsLink');
+
+// Check if the element is indeed an HTMLAnchorElement (anchor tag)
+if(productsLink instanceof HTMLAnchorElement) {
+    productsLink.addEventListener('click', function(event) {
+        event.preventDefault();  
+
+        fetch('/api/products')
+            .then((response: Response) => {
+                if (!response.ok) {
+                    throw new Error('Server Error');
+                }
+                return response.json();
+            })
+            .then((data: any):void => {
+                console.log(data)
+            })
+            .catch((error: Error) => {
+                console.error('Error:', error.message);
+            });
+    });
+} else {
+    console.error('Link (front-end) is not working!');
+}
