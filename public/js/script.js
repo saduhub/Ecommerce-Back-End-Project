aa1dce4fcb1854fe332fdea8dd81a7ebf8af93fa
Jorge Duarte <sadujorge@gmail.com>
2024-01-04 20:12:13 -0800

    Added interfaces to ensure data received and processed is the correct data type - Array of product objects

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,9 @@
 "use strict";
 const productsLink = document.getElementById('productsLink');
-// Check if the element is indeed an HTMLAnchorElement (anchor tag)
+function createProductCards(data) {
+    console.log(data);
+}
+// Check if the element is indeed an HTMLAnchorElement (anchor tag). Can add loading indicator function to show and hide loading message while code excecutes.
 if (productsLink instanceof HTMLAnchorElement) {
     productsLink.addEventListener('click', function (event) {
         event.preventDefault();
@@ -12,7 +15,8 @@ if (productsLink instanceof HTMLAnchorElement) {
             return response.json();
         })
             .then((data) => {
-            console.log(data);
+            // console.log(data)
+            createProductCards(data);
         })
             .catch((error) => {
             console.error('Error:', error.message);
--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,39 @@
 const productsLink = document.getElementById('productsLink');
 
-// Check if the element is indeed an HTMLAnchorElement (anchor tag)
+interface ProductTag {
+    id: number;
+    product_id: number;
+    tag_id: number;
+}
+
+interface Tag {
+    id: number;
+    tag_name: string;
+    product_tag?: ProductTag;
+}
+
+interface Product {
+    id: number;
+    product_name: string;
+    price: number;
+    stock: number;
+    category_id: number;
+    category: {
+        id: number;
+        category_name: string;
+    };
+    tags: Tag[];
+}
+
+// The response would be an array of product objects. Each product should also include category and tag information if available.
+type ProductsResponse = Product[];
+
+
+function createProductCards(data: ProductsResponse) {
+    console.log(data)
+}
+
+// Check if the element is indeed an HTMLAnchorElement (anchor tag). Can add loading indicator function to show and hide loading message while code excecutes.
 if(productsLink instanceof HTMLAnchorElement) {
     productsLink.addEventListener('click', function(event) {
         event.preventDefault();  
@@ -12,8 +45,9 @@ if(productsLink instanceof HTMLAnchorElement) {
                 }
                 return response.json();
             })
-            .then((data: any):void => {
-                console.log(data)
+            .then((data: ProductsResponse):void => {
+                // console.log(data)
+                createProductCards(data);
             })
             .catch((error: Error) => {
                 console.error('Error:', error.message);
